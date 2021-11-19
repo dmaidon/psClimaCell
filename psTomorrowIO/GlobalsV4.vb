13689b233b61fd4b17bc87bb5b8655787859e083
Dennis Maidon <dmaidon@dmaidon.com>
2021-11-19 07:01:17 -0500

    Rearrange

--- psTomorrowIO/GlobalsV4.vb
+++ psTomorrowIO/GlobalsV4.vb
@@ -2,62 +2,85 @@
 
 Friend Module GlobalsV4
 
-    'Folder declarations
-    Public TempDir As String = Path.Combine(Application.StartupPath, "$tmp")
-    Public DataDir As String = Path.Combine(Application.StartupPath, "Data")
-    Public LogDir As String = Path.Combine(Application.StartupPath, "Logs")
-    Public IconDir As String = Path.Combine(Application.StartupPath, "Icons")
-    Public LocationDir As String = Path.Combine(Application.StartupPath, "Locations")
-    Public ImageDir As String = Path.Combine(Application.StartupPath, "ForecastImages")
+#Region "Folders"
 
-    Public LogFile, TlDataFile, ErrFile As String
+    Public DataDir As String = Path.Combine(Application.StartupPath, "Data")
 
     Public DayDir, MonthDir, YearDir As String
 
-    Public Cpy As String = $"©2020-{Now:yyyy} PAROLE Software - All rights reserved."
+    Public IconDir As String = Path.Combine(Application.StartupPath, "Icons")
+
+    Public ImageDir As String = Path.Combine(Application.StartupPath, "ForecastImages")
+
+    Public LocationDir As String = Path.Combine(Application.StartupPath, "Locations")
+
+    Public LogDir As String = Path.Combine(Application.StartupPath, "Logs")
+
+    Public LogFile, TlDataFile, ErrFile As String
+
+    Public TempDir As String = Path.Combine(Application.StartupPath, "$tmp")
+
+#End Region
+
+#Region "Log Variables"
 
     Public GotErr As Boolean
     Public ResetErr As Boolean
+
+#End Region
+
+#Region "Data Variables"
+
     Public FetchWildfire As Boolean
     Public SaveFcImages As Boolean
 
+    Public suffixArr As New List(Of String)({"Max", "Min", "Avg", "MaxTime", "MinTime"})
     Public unitArr As New List(Of String)({"imperial", "metric"})
     Public unitNfo As FieldUnitsModel
 
-    ' Timelines update interval
-    Public TlDuration As TimeSpan
-    Public TlNextUpdate As DateTime
+#End Region
 
-    ' download reset timer interval
-    Public ResetDuration As TimeSpan
-    Public ResetNextUpdate As DateTime
-    Public ResetCounter As Integer
-
+#Region "Timer Variables"
 
     ' Midnight interval
     Public MidDuration As TimeSpan
+
     Public MidNextUpdate As DateTime
+
+    Public ResetCounter As Integer
+
+    ' download reset timer interval
+    Public ResetDuration As TimeSpan
+
+    Public ResetNextUpdate As DateTime
+
+    ' Timelines update interval
+    Public TlDuration As TimeSpan
+
+    Public TlNextUpdate As DateTime
+
+#End Region
+
+#Region "Weather Icons"
+
+    Public ImgPbArr As New List(Of PictureBox)({FrmMainv4.PbImgStyle0, FrmMainv4.PbImgStyle1, FrmMainv4.PbImgStyle2, FrmMainv4.PbImgStyle3})
+    Public ImgSamp As New List(Of String)({"1000.png", "1102.png", "4000.png", "5001.png"})
+    Public ImgStyleArr As New List(Of String)({"A", "B"})
+
+#End Region
+
+#Region "App Variables"
+
+    'time value for when application starts up
+    Public AppStartTime As DateTime
+
+    Public Cpy As String = $"©2020-{Now:yyyy} PAROLE Software - All rights reserved."
 
     'number of times that program has been run
     Public Timesrun As Long
 
     Public Use_Agent As String = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63"
 
+#End Region
 
-    Public suffixArr As New List(Of String)({"Max", "Min", "Avg", "MaxTime", "MinTime"})
-    Public ImgPbArr As New List(Of PictureBox)({FrmMainv4.PbImgStyle0, FrmMainv4.PbImgStyle1, FrmMainv4.PbImgStyle2, FrmMainv4.PbImgStyle3})
-
-    Public ImgStyleArr As New List(Of String)({"A", "B"})
-    Public ImgSamp As New List(Of String)({"1000.png", "1102.png", "4000.png", "5001.png"})
-
-    'Public Enum PreX
-    '    Max = &H1
-    '    Min = &H2
-    '    Avg = &H4
-    '    MaxTime = &H8
-    '    MinTime = &H10
-    'End Enum
-
-    'time value for when application starts up
-    Public AppStartTime As DateTime
 End Module
